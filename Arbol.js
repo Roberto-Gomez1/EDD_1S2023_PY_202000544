1c0b1604742a483bf8a9dda6f109e2a39553337a
Roberto-Gomez1 <nuevoz9c@gmail.com>
2023-04-10 19:34:17 -0600

    Creacion de estudiantes

---
+++ Arbol.js
@@ -0,0 +1,216 @@
+class nodoArbol {
+    constructor(valor){
+        this.izquierdo = null;
+        this.derecho = null;
+        this.valor = valor;
+    }
+}
+
+class Arbol {
+    constructor(){
+        this.raiz = null;
+    }
+ 
+    insertarNodo(nodo, raiz){
+        if(raiz === null){
+            raiz = nodo;
+        }else{
+            if(raiz.valor === nodo.valor){
+                raiz.valor = nodo.valor
+            }else if(raiz.valor < nodo.valor){
+                raiz.derecho = this.insertarNodo(nodo, raiz.derecho)
+            }else{
+                raiz.izquierdo = this.insertarNodo(nodo, raiz.izquierdo)
+            }
+        }
+        return raiz;
+    }
+
+    insertarValor(valor){
+        const nuevoNodo = new nodoArbol(valor);
+        this.raiz = this.insertarNodo(nuevoNodo, this.raiz);
+    }
+
+    recorridoPreorden(raiz){
+        var cadena = ""
+        if(raiz !== null){
+            cadena = cadena + "\""
+            cadena = cadena + raiz.valor
+            cadena = cadena + "\""
+            if(raiz.izquierdo !== null){
+                cadena = cadena + " -> "
+                cadena = cadena + this.recorridoPreorden(raiz.izquierdo)
+            }
+            if(raiz.derecho !== null){
+                cadena = cadena + " -> "
+                cadena = cadena + this.recorridoPreorden(raiz.derecho)
+            }
+        }
+        return cadena
+    }
+
+    recorridoInorden(raiz){
+        var cadena = ""
+        if(raiz !== null){
+            if(raiz.izquierdo !== null){
+                cadena += this.recorridoInorden(raiz.izquierdo)
+                cadena += " -> "
+            }
+            cadena += "\""
+            cadena += raiz.valor
+            cadena += "\""
+            if(raiz.derecho !== null){
+                cadena += " -> "
+                cadena += this.recorridoInorden(raiz.derecho)
+            }
+        }
+        return cadena
+    }
+
+    recorridoPostOrden(raiz){
+        var cadena = ""
+        if(raiz !== null){
+            if(raiz.izquierdo !== null){
+                cadena += this.recorridoPostOrden(raiz.izquierdo)
+                cadena += " -> "
+            }
+            if(raiz.derecho !== null){
+                cadena += this.recorridoPostOrden(raiz.derecho)
+                cadena += " -> "
+            }
+            cadena += "\""
+            cadena += raiz.valor
+            cadena += "\""
+        }
+        return cadena
+    }
+
+    recorridoArbol(){
+        console.log(this.recorridoInorden(this.raiz))
+        console.log(this.recorridoPreOrden(this.raiz))
+        console.log(this.recorridoPostOrden(this.raiz))
+    }
+
+    retornarValoresArbol(raiz, id){
+        var cadena = "";
+        var numero = id + 1;
+        if(raiz !== null){
+            cadena += "\"";
+            cadena += raiz.valor;
+            cadena += "\" ;";
+            if(!(raiz.izquierdo === null) && !(raiz.derecho === null)){
+                cadena += " x" + numero + " [label=\"\",width=.1,style=invis];"
+                cadena += "\"";
+                cadena += raiz.valor;
+                cadena += "\" -> ";
+                cadena += this.retornarValoresArbol(raiz.izquierdo, numero)
+                cadena += "\"";
+                cadena += raiz.valor;
+                cadena += "\" -> ";
+                cadena += this.retornarValoresArbol(raiz.derecho, numero)
+                cadena += "{rank=same" + "\"" + raiz.izquierdo.valor + "\"" + " -> " + "\"" + raiz.derecho.valor + "\""  + " [style=invis]}; "
+            }else if(!(raiz.izquierdo === null) && (raiz.derecho === null)){
+                cadena += " x" + numero + " [label=\"\",width=.1,style=invis];"
+                cadena += "\"";
+                cadena += raiz.valor;
+                cadena += "\" -> ";
+                cadena += this.retornarValoresArbol(raiz.izquierdo, numero)
+                cadena += "\"";
+                cadena += raiz.valor;
+                cadena += "\" -> ";
+                cadena += "x" + numero + "[style=invis]";
+                cadena += "{rank=same" + "\"" + raiz.izquierdo.valor + "\"" + " -> " + "x" + numero + " [style=invis]}; "
+            }else if((raiz.izquierdo === null) && !(raiz.derecho === null)){
+                cadena += " x" + numero + " [label=\"\",width=.1,style=invis];"
+                cadena += "\"";
+                cadena += raiz.valor;
+                cadena += "\" -> ";
+                cadena += "x" + numero + "[style=invis]";
+                cadena += "; \"";
+                cadena += raiz.valor;
+                cadena += "\" -> ";
+                cadena += this.retornarValoresArbol(raiz.derecho, numero)
+                cadena += "{rank=same" + " x" + numero + " -> \"" + raiz.derecho.valor + "\"" +  " [style=invis]}; "
+            }
+        }
+        return cadena;
+    }
+
+    graficarArbol(){
+        var cadena = ""
+        if(this.raiz !== null){
+            cadena += "digraph arbol {"
+            cadena += this.retornarValoresArbol(this.raiz, 0)
+            cadena += "}"
+        }
+        return cadena
+    }
+
+    /** 
+     * Contenido de graficar los diferentes recorridos del arbol
+     */
+    recorridosArbol(){
+        console.log("Recorrido Pre-Orden")
+        let url = 'https://quickchart.io/graphviz?graph=';
+        let body = "digraph G { graph[label = \"Pre-Orden\" rankdir = LR labelloc = t]" + this.recorridoPreorden(this.raiz) + "}";
+        $("#image1").attr("src", url + body);
+        console.log("Recorrido In-Orden")
+        body = "digraph G { graph[label = \"In-Orden\" rankdir = LR labelloc = t]" + this.recorridoInorden(this.raiz) + "}";
+        $("#image2").attr("src", url + body);
+        console.log("Recorrido Post-Orden")
+        body = "digraph G { graph[label = \"Post-Orden\" rankdir = LR labelloc = t]" + this.recorridoPostOrden(this.raiz) + "}";
+        $("#image3").attr("src", url + body);
+    }
+
+    eliminarTodo(){
+        this.raiz = null;
+    }
+
+}
+
+
+const arbolBinario = new Arbol();
+
+function agregarVarios(){
+    let valor = document.getElementById("valor").value
+    let valores = valor.split(',')
+    try{
+        valores.forEach(element => {
+            arbolBinario.insertarValor(element)
+        });
+    }catch(error){
+        alert("Hubo un error")
+    }
+    refrescarArbol()
+}
+
+function agregarVariosNumeros(){
+    let valor = document.getElementById("valor").value
+    let valores = valor.split(',')
+    try{
+        valores.forEach(element => {
+            arbolBinario.insertarValor(parseInt(element))
+        });
+    }catch(error){
+        alert("Hubo un error")
+    }
+    refrescarArbol();
+}
+
+function refrescarArbol(){
+    let url = 'https://quickchart.io/graphviz?graph=';
+    let body = arbolBinario.graficarArbol();
+    $("#image").attr("src", url + body);
+    document.getElementById("valor").value = "";
+}
+
+function recorrerArbol(){
+    arbolBinario.recorridosArbol();
+}
+
+function limpiar(){
+    arbolBinario.eliminarTodo();
+    let url = 'https://quickchart.io/graphviz?graph=digraph G { raiz }';
+    $("#image").attr("src", url);
+    document.getElementById("valor").value = "";
+}
---
+++ Objeto.js
@@ -0,0 +1,155 @@
+class Alumno{
+    constructor(nombre,carnet, contraseña,carpeta){
+        this.nombre = nombre;
+        this.carnet = carnet;
+        this.contraseña = contraseña;
+        this.carpeta = carpeta;
+    }
+}
+
+let alumnos = [];
+
+function cargaAlumnos(e) {
+    var archivo = e.target.files[0];
+
+    if (!archivo) {
+        return;
+    }
+
+    let lector = new FileReader();
+    lector.onload = function(e) {
+        let contenido = e.target.result;
+
+        const object = JSON.parse(contenido);
+
+        alumnos = object.alumnos.map((alumno) => {
+            return new Alumno(alumno.nombre, alumno.carnet, alumno.password, alumno.Carpeta_Raiz);
+          });
+        for (let i = 0; i < alumnos.length; i++) {
+            console.log(alumnos[i]);
+        }
+        
+    }
+    lector.readAsText(archivo);
+}
+
+document.getElementById("carga_alumnos").addEventListener("change", cargaAlumnos, false);
+
+document.getElementById("select-options").addEventListener("change", function(){
+    const selectedOption = this.value;
+    const container = document.getElementById("alumnos-container");
+    if (selectedOption === "option1") {
+        container.innerHTML = "";
+        alumnos.forEach((alumno) => {
+            const card = document.createElement("div");
+            card.classList.add("col-xl-3", "col-md-6", "mb-4");
+            card.innerHTML = `
+                <div class="card border-left-primary shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                    Nombre: ${alumno.nombre}</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                    Carnet: ${alumno.carnet}
+                                </div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                    Carpeta: ${alumno.carpeta}
+                                </div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-user fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `;
+            container.appendChild(card);
+        });
+    } else if (selectedOption === "option2") {
+        container.innerHTML = "";
+        let index = 0;
+        
+        const createCard = (alumno) => {
+          const card = document.createElement("div");
+          card.classList.add("col-xl-3", "col-md-6", "mb-4");
+          card.innerHTML = `
+            <div class="card border-left-primary shadow h-100 py-2">
+              <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                  <div class="col mr-2">
+                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                      Nombre: ${alumno.nombre}
+                    </div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800">
+                      Carnet: ${alumno.carnet}
+                    </div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800">
+                      Carpeta: ${alumno.carpeta}
+                    </div>
+                  </div>
+                  <div class="col-auto">
+                    <i class="fas fa-user fa-2x text-gray-300"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          `;
+          container.appendChild(card);
+        };
+        
+        const createCards = (alumnos) => {
+          if (!alumnos || alumnos.length === 0) {
+            return;
+          }
+          createCard(alumnos[index++]);
+          createCards(alumnos[index - 1].hijos);
+          if (index === alumnos.length) {
+            return;
+          }
+          createCards(alumnos);
+        };
+        
+        createCards(alumnos);
+      }else if (selectedOption === "option3") {
+        container.innerHTML = "";
+        
+        const createPostOrderCards = (alumnos) => {
+          if (!alumnos || alumnos.length === 0) {
+            return;
+          }
+      
+          const [left, right] = [alumnos.filter((_, i) => i < alumnos.length - 1), alumnos[alumnos.length - 1]];
+      
+          const card = document.createElement("div");
+          card.classList.add("col-xl-3", "col-md-6", "mb-4");
+          card.innerHTML = `
+            <div class="card border-left-primary shadow h-100 py-2">
+              <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                  <div class="col mr-2">
+                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                      Nombre: ${right.nombre}
+                    </div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800">
+                      Carnet: ${right.carnet}
+                    </div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800">
+                      Carpeta: ${right.carpeta}
+                    </div>
+                  </div>
+                  <div class="col-auto">
+                    <i class="fas fa-user fa-2x text-gray-300"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          `;
+          container.appendChild(card);
+          
+          createPostOrderCards(left);
+        };
+        
+        createPostOrderCards(alumnos);
+      }
+});
